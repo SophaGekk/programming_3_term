109b7c1b6bfc503a2d65a8f03395084237c758b0
SophaGekk <168344741+SophaGekk@users.noreply.github.com>
2024-09-10 15:57:00 +0700

    Hello world!

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main (int, char **) {
+    // std::cout << "Version: " << version() << std::endl;
+    // std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world! Version 1.0." << version() << std::endl;
+
+    return 0;
+}
